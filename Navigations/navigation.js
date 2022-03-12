9e6d6cb55ebba1d68610f96666227e257f8394b9
Bill Zack <bzak954@gmail.com>
2022-03-12 19:59:17 +0000

    add new screens

--- Navigations/navigation.js
+++ Navigations/navigation.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Main from "../Screens/Main";
+import DepositScreen from "../Screens/DepositScreen";
+import SendMoneyScreen from "../Screens/SendMoneyScreen";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,8 @@ function StackNav() {
         }}
       >
         <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Deposit" component={DepositScreen} />
+        <Stack.Screen name="SendMoney" component={SendMoneyScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
