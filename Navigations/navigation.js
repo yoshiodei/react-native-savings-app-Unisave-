ac3af5e06e25bf840e07843d968b545b2fa065bb
Bill Zack <bzak954@gmail.com>
2022-03-13 20:44:00 +0000

    add new screens to navigator

--- Navigations/navigation.js
+++ Navigations/navigation.js
@@ -4,6 +4,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import Main from "../Screens/Main";
 import DepositScreen from "../Screens/DepositScreen";
 import SendMoneyScreen from "../Screens/SendMoneyScreen";
+import QRCodeScreen from "../Screens/QRCodeScreen";
+import TransactionsScreens from "../Screens/TransactionsScreen";
+import EditPasswordScreen from "../Screens/EditPasswordScreen";
+import SetPinScreen from "../Screens/SetPinScreen";
+import WithdrawScreen from "../Screens/WithdrawScreen";
+import RequestLoan from "../Screens/RequestLoan";
 
 const Stack = createStackNavigator();
 
@@ -18,6 +24,12 @@ function StackNav() {
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="Deposit" component={DepositScreen} />
         <Stack.Screen name="SendMoney" component={SendMoneyScreen} />
+        <Stack.Screen name="QRCode" component={QRCodeScreen} />
+        <Stack.Screen name="Transactions" component={TransactionsScreens} />
+        <Stack.Screen name="EditPassword" component={EditPasswordScreen} />
+        <Stack.Screen name="SetPin" component={SetPinScreen} />
+        <Stack.Screen name="Withdraw" component={WithdrawScreen} />
+        <Stack.Screen name="RequestLoan" component={RequestLoan} />
       </Stack.Navigator>
     </NavigationContainer>
   );
