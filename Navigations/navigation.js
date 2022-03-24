e4c88806abfc25a37e42e60885251df54ca48587
Bill Zack <bzak954@gmail.com>
2022-03-24 18:59:26 +0000

    add new screens to navigator

--- Navigations/navigation.js
+++ Navigations/navigation.js
@@ -10,28 +10,32 @@ import EditPasswordScreen from "../Screens/EditPasswordScreen";
 import SetPinScreen from "../Screens/SetPinScreen";
 import WithdrawScreen from "../Screens/WithdrawScreen";
 import RequestLoan from "../Screens/RequestLoan";
+import IntermediateLoan from "../Screens/InterMediateLoan";
+import StandardLoan from "../Screens/StandardLoan";
+import PremiumLoan from "../Screens/PremiumLoan";
 
 const Stack = createStackNavigator();
 
 function StackNav() {
   return (
-    
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="Deposit" component={DepositScreen} />
-        <Stack.Screen name="SendMoney" component={SendMoneyScreen} />
-        <Stack.Screen name="QRCode" component={QRCodeScreen} />
-        <Stack.Screen name="Transactions" component={TransactionsScreens} />
-        <Stack.Screen name="EditPassword" component={EditPasswordScreen} />
-        <Stack.Screen name="SetPin" component={SetPinScreen} />
-        <Stack.Screen name="Withdraw" component={WithdrawScreen} />
-        <Stack.Screen name="RequestLoan" component={RequestLoan} />
-      </Stack.Navigator>
-    
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Deposit" component={DepositScreen} />
+      <Stack.Screen name="SendMoney" component={SendMoneyScreen} />
+      <Stack.Screen name="QRCode" component={QRCodeScreen} />
+      <Stack.Screen name="Transactions" component={TransactionsScreens} />
+      <Stack.Screen name="EditPassword" component={EditPasswordScreen} />
+      <Stack.Screen name="SetPin" component={SetPinScreen} />
+      <Stack.Screen name="Withdraw" component={WithdrawScreen} />
+      <Stack.Screen name="RequestLoan" component={RequestLoan} />
+      <Stack.Screen name="Intermediate" component={IntermediateLoan} />
+      <Stack.Screen name="Standard" component={StandardLoan} />
+      <Stack.Screen name="Premium" component={PremiumLoan} />
+    </Stack.Navigator>
   );
 }
 
